1d88bfaf36726b44fc8e995e6fc65c7ac4bfdc87
GoggleEyed <GoggleEyed@protonmail.com>
2022-04-03 16:58:46 +0300

    add feature authentication

--- src/hippodrome.c
+++ src/hippodrome.c
@@ -1,5 +1,50 @@
-#include "../include/hippodrome.h"
+#include "../inc/hippodrome.h"
 
-void PrintHello() {
-    printf("Hello, World!\n");
+static int callback_user_type(void *data, int argc, char **argv, char **azColName) {
+  int i;
+  for (i = 0; i < argc; i++) {
+	printf("%s ", argv[i] ? argv[i] : "(null)");
+  }
+  printf("\n");
+  return 0;
+}
+
+static void SQL_Error(int rc, char *zErrMsg) {
+  if (rc != SQLITE_OK) {
+	fprintf(stderr, "SQL error: %s\n", zErrMsg);
+	sqlite3_free(zErrMsg);
+  }
+}
+
+void Authentication(sqlite3 *db) {
+  char *zErrMsg = 0;
+  char *sql;
+  printf("Choose user type:\n");
+  sql = "SELECT * FROM user_type";
+  int rc;
+  rc = sqlite3_exec(db, sql, callback_user_type, 0, &zErrMsg);
+  SQL_Error(rc, zErrMsg);
+  int user_type;
+  scanf("%d", &user_type);
+  printf("Enter your surname:\n");
+  char surname[30];
+  scanf("%s", surname);
+  printf("Enter your password:\n");
+  char password[30];
+  scanf("%s", password);
+  sql = "INSERT INTO users (user_type, surname, password) VALUES (?, ?, ?);";
+
+  sqlite3_stmt *res;
+  rc = sqlite3_prepare_v2(db, sql, -1, &res, 0);
+
+  if (rc == SQLITE_OK) {
+	sqlite3_bind_int(res, 1, user_type);
+	sqlite3_bind_text(res, 2, surname, -1, 0);
+	sqlite3_bind_text(res, 3, password, -1, 0);
+  } else {
+	fprintf(stderr, "Failed to execute statement: %s\n", sqlite3_errmsg(db));
+  }
+
+  sqlite3_step(res);
+  sqlite3_finalize(res);
 }
